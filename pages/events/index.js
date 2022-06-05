b1606c1ba9c33ba6fa94e0ccc9afd28ade80cf15
liuxd <17602237394@163.com>
2022-06-05 21:22:54 +0800

    feat:change event page by use ssg

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,17 +1,26 @@
-import { getAllEvents } from '../../dummy-data';
+import { getAllEvents } from '../../helpers/apiUtils';
 import EventList from '../../components/events/EventtList';
 import EventSearch from '../../components/events/EventSearch';
 import { useRouter } from 'next/router';
 
-const AllEventsPage = () => {
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  }
+}
 
-  const router = useRouter()
-  const events = getAllEvents();
+const AllEventsPage = ({ events }) => {
+  const router = useRouter();
 
   const findEventsHandle = (year, month) => {
     const fullPath = `/events/${year}/${month}`;
     router.push(fullPath);
   }
+
   return (
     <div className='w-full flex flex-col items-center justify-center'>
       <EventSearch onSearch={findEventsHandle} />
