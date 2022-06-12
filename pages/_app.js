36c11f852a4c5be0d4d0aa5ada445f6d25d22cdf
liuxd <17602237394@163.com>
2022-06-12 14:29:49 +0800

    feat:  add all page meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Layout from '../components/layout/layout'
 import '../styles/globals.css'
 // import 'tailwindcss/tailwind.css'
@@ -5,6 +6,11 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <title>next study</title>
+        <meta name='description' content='next.js study' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
