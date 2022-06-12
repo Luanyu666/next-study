ad12867ea1b32877cf4a6216b77a96d96f591879
liuxd <17602237394@163.com>
2022-06-12 13:56:27 +0800

    test

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,46 +1,93 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 import EventList from "../../components/events/EventtList";
 import Button from "../../components/ui/Button";
-import { getFilteredEvents } from "../../helpers/apiUtils";
+// import { getFilteredEvents } from "../../helpers/apiUtils";
 
-export async function getServerSideProps(context) {
-  const { params } = context;
+// export async function getServerSideProps(context) {
+//   const { params } = context;
 
-  const filterData = params.slug;
+//   const filterData = params.slug;
 
+//   const year = +filterData[0];
+//   const month = +filterData[1];
+
+//   if (isNaN(year) || isNaN(month) || year > 2030 || month < 1 || month > 12) {
+//     return {
+//       props: {
+//         hasError: true,
+//       },
+//       notFound: true,
+//     };
+//   }
+
+//   const filteredEvents = await getFilteredEvents({ year, month });
+
+//   return {
+//     props: {
+//       filteredEvents,
+//       year,
+//       month,
+//     },
+//   };
+// }
+
+const FilterEventsPage = (props) => {
+  const router = useRouter();
+  const filterData = router.query.slug;
+  const [events, setEvents] = useState([]);
+
+  const { data, error } = useSWR(
+    "https://next-study-1952f-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
+  );
+
+  useEffect(() => {
+    if (data) {
+      const events = [];
+
+      for (const key in data) {
+        events.push({
+          id: key,
+          ...data[key],
+        });
+      }
+      setEvents(events);
+    }
+  }, [data]);
+
+  if (!events) {
+    return (
+      <p className="">
+        <svg
+          className="animate-spin h-5 w-5 mr-3 ..."
+          viewBox="0 0 24 24"
+        ></svg>
+        loading....
+      </p>
+    );
+  }
+
+  console.log(filterData, events, data, router)
   const year = +filterData[0];
   const month = +filterData[1];
 
-  if (isNaN(year) || isNaN(month) || year > 2030 || month < 1 || month > 12) {
-    return {
-      props: {
-        hasError: true,
-      },
-      notFound: true
-    };
+  if (isNaN(year) || isNaN(month) || error || year > 2030 || month < 1 || month > 12) {
+    return (
+      <div className="w-full flex flex-col items-center">
+        <p className="select-none rounded-xl font-bold bg-blue-300 w-3/5 h-20 m-2 flex justify-center items-center">
+          invalid filter, please adjust values
+        </p>
+        <Button link="/events">show all events</Button>
+      </div>
+    );
   }
-
-  const filteredEvents = await getFilteredEvents({ year, month });
-
-  return {
-    props: {
-      filteredEvents,
-      year,
-      month,
-    },
-  };
-}
-
-const FilterEventsPage = ({
-  hasError = false,
-  filteredEvents,
-  year,
-  month,
-}) => {
-
-  if (hasError) {
-    return <p>invalid filter</p>;
-  }
+  const filterEvents = events.filter((e) => {
+    const eventDate = new Date(e.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
 
   const date = new Date(year, month - 1);
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -49,7 +96,7 @@ const FilterEventsPage = ({
     year: "numeric",
   });
 
-  if (!filteredEvents || filteredEvents.length === 0) {
+  if (!filterEvents || filterEvents.length === 0) {
     return (
       <div className="w-full flex flex-col items-center">
         <p className="select-none rounded-xl font-bold bg-blue-300 w-3/5 h-20 m-2 flex justify-center items-center">
@@ -65,7 +112,7 @@ const FilterEventsPage = ({
         <h1 className="my-2 font-bold">Events in {humanReadableDate}</h1>
         <Button link="/events">show all events</Button>
       </section>
-      <EventList items={filteredEvents} />
+      <EventList items={filterEvents} />
     </div>
   );
 };
